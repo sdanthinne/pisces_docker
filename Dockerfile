9ec7b1fdd6719d1f52878d98ed4755c7003c93b4
Sebastien Danthinne <sdanthinne@gmail.com>
2023-03-22 03:00:12 +0000

    Working prototype now

--- Dockerfile
+++ Dockerfile
@@ -4,16 +4,26 @@ FROM tiangolo/uwsgi-nginx-flask:python3.8
 RUN apt update
 RUN apt install -y ntp ca-certificates
 RUN wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb 
+RUN git clone https://github.com/Illumina/Pisces.git
 RUN dpkg -i packages-microsoft-prod.deb
 RUN apt update
 RUN apt install -y apt-transport-https
-RUN apt install -y dotnet-runtime-2.1
+RUN apt install -y dotnet-runtime-2.1 rename
 RUN echo "$CACHEBUST"
+RUN pip install eventlet shelljob flask_wtf Flask-BasicAuth
 ARG CACHEBUST=1
 COPY ./app /app
-COPY ./Pisces ./Pisces
-RUN ls -tdr Pisces/binaries/* | head -1 > file_loc
-RUN cp  $(cat file_loc)/* .
-RUN ls
-RUN ls *.gz | xargs -n1 tar -xzf
+ARG VERSION=5.2.11.163
+RUN tar -xzf Pisces/binaries/$VERSION/Pisces_$VERSION.tar.gz
+RUN tar -xzf Pisces/binaries/$VERSION/CreateGenomeSizeFile_$VERSION.tar.gz
+
+#COPY ./Pisces /Pisces
+#RUN ls -tdr /Pisces/binaries/* | head -1 > file_loc
+#RUN mkdir bin
+#RUN cp  $(cat file_loc)/* bin/.
+#RUN ls -d bin/*
+#RUN ls -d bin/*.gz | xargs -I {} tar -xzf {} -C bin/
+#RUN rm -r bin/*.gz
+#RUN for file in bin/*; do mv "$file" "$(echo "$file" | sed 's/\_.*//')" ; done
+#RUN rename -f 's/\_.*//' bin/*
 
--- app/main.py
+++ app/main.py
@@ -1,51 +1,107 @@
-import os
-from flask import Flask, flash, request, redirect, render_template
+
+from flask import Flask,render_template, send_from_directory, url_for, redirect, session
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired
 from werkzeug.utils import secure_filename
+from pathlib import Path
+from flask_basicauth import BasicAuth
+from time import sleep
 
-app=Flask(__name__)
+from wtforms import StringField, TextAreaField, SubmitField, MultipleFileField
+from wtforms.validators import DataRequired,Email, ValidationError
+import subprocess
+from threading import Thread
 
-app.secret_key = "secret key"
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = 'this is the way the world started. In chaos'
+    app.config["BASIC_AUTH_USERNAME"] = 'od260'
+    app.config["BASIC_AUTH_PASSWORD"] = 'od260ONLY'
 
-# Get current path
-path = os.getcwd()
-# file Upload
-UPLOAD_FOLDER = os.path.join(path, 'uploads')
+    AUTH_HASH='pbkdf2:sha256:260000$AAwUgjbKNucc80Sj$6e15d7de7a0010800f87bfd208b62ae0182b8d9c8a02b76c1b6720fd7a38ff00'
+    app.secret_key = AUTH_HASH
+    return app
 
-# Make directory if uploads is not exists
-if not os.path.isdir(UPLOAD_FOLDER):
-    os.mkdir(UPLOAD_FOLDER)
+app = create_app()
+basic_auth = BasicAuth(app)
 
-    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+def has_files(form, files):
+    name_types = [".bam",".bam.bai",".fa"]
+    for file in form.files.data:
+        matched = False
+        for name in name_types:
+            secure_fname = secure_filename(file.filename)
+            if name in secure_fname:
+                name_types.remove(name)
+                new_fname = secure_filename(form.jobname.data)
+                Path(f"tmp/{new_fname}").mkdir(parents=True, exist_ok=True)
+                file.save(f'''tmp/{new_fname}/{new_fname}{name}''')
+                matched = True
+        if not matched:
+            raise ValidationError
 
-    # Allowed extension you can set your own
-    ALLOWED_EXTENSIONS = set(['fa', 'bai', 'bam'])
 
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+class PiscesForm(FlaskForm):
+    jobname = StringField("Job Name", validators=[DataRequired()])
+    files = MultipleFileField(validators=[has_files], render_kw={'multiple': True})
+    submit = SubmitField("Submit Job",render_kw={"onclick":"loading();"})
+
+@app.route("/download/<path:path>")
+def send_results(path):
+    return send_from_directory('tmp',path)
+
+def process_files(jobname):
+    FILE_DIR=f"tmp/{jobname}"
+    CMD_CREATEGENOMESIZEFILE=f"dotnet CreateGenomeSizeFile/CreateGenomeSizeFile.dll -g {FILE_DIR}/ -o {FILE_DIR}/ -s \"Not Valid (not valid)\""
+    CMD_PISCES=f"dotnet Pisces/Pisces.dll -Bam {FILE_DIR}/{jobname}.bam -G {FILE_DIR}/ -OutFolder {FILE_DIR}/ -CallMNVs false -gVCF false -RMxNFilter 5,9,0.35 -MinimumFrequency 0.01 -threadbychr true"
+    with open(f"{FILE_DIR}/commands_used.txt","w+") as f:
+        f.writelines([CMD_CREATEGENOMESIZEFILE, f"\n{CMD_PISCES}"])
+        f.close()
+    subcmd_creategenomesizefile = subprocess.run(CMD_CREATEGENOMESIZEFILE, shell=True, text=True)
+    if subcmd_creategenomesizefile.returncode !=0:
+        return False
+    cmd = subprocess.run(CMD_PISCES, shell=True,text=True)
+    return f"{cmd.stdout}\n{cmd.stderr}",True if cmd.returncode == 0 else False
+
+@app.route("/tmp/<string:jobname>")
+def display_results(jobname):
+    try:
+        open(f"tmp/{jobname}/done")
+    except FileNotFoundError:
+        return render_template("job_results.html",output_message="",jobname=jobname,done=False)
+    
+    commands = open(f'tmp/{jobname}/commands_used.txt').readlines()
+    commands = "<br/>".join(commands)
+    output_message=f"Processing completed, please click link below to download. These results will be saved for 1 day online at this URL. The commands used were:\n <br/> {commands}"
+    return render_template("job_results.html",output_message=output_message,jobname=jobname,done=True)
+
+class PiscesThread(Thread):
+    def __init__(self,jobname,*args,**kwargs):
+        Thread.__init__(self,*args,**kwargs)
+        self.jobname = jobname
+        self.output = ()
+        self.done = False
+    def run(self):
+        self.output = process_files(self.jobname)
+        self.done = True
+        path = f"tmp/{self.jobname}"
+        with open(f"{path}/done", "w+") as df:
+            df.close()
+        
 
 
-@app.route('/')
-def upload_form():
-    return render_template('upload.html')
-@app.route('/', methods=['POST'])
-def upload_file():
-    if request.method == 'POST':
-        if 'files[]' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        files = request.files.getlist('files[]')
-        f_names = []
-        for file in files:
-            if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                f_names.append(filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        flash(f'Successfully uploaded files: {f_names}')
-        if len(f_names) == 3:
-            flash("processing")
-        else:
-            flash("Incorrect number of files, cannot process.\n")
-        return redirect('/')
+@app.route("/",methods=['GET','POST'])
+@basic_auth.required
+def contact():
+    form = PiscesForm()
+    if form.validate_on_submit():
+        jobname = form.jobname.data
+        t = PiscesThread(target=process_files,jobname=jobname)
+        t.start()
+        return redirect(url_for(f"display_results",jobname=jobname))
 
+    return render_template("start.html",form=form)
+
+if __name__=="__main__":
+    app.run(debug=True)
